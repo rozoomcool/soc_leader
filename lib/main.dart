2435af109c4a634e41642cc62fdc737bac463c49
rozoomcool <rozoom.um@gmail.com>
2023-11-05 07:07:42 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:soc_leader/presentation/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'application/application.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
